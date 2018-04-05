c11abeea73295bf09ca7e0a927a8a7f25453421a
somkiat <somkiat.p@gmail.com>
2018-04-05 10:01:56 +0700

    Demo of swagger with python

---
+++ sample_service/api/__init__.py
@@ -0,0 +1,1 @@
+from api.product import Product
---
+++ sample_service/api/product.py
@@ -0,0 +1,12 @@
+products = {
+    0: {
+    "name": "First product",
+    "price": 100
+    }
+}
+
+class Product(object):
+    def search(self) -> list:
+        return products
+
+class_instance = Product()
